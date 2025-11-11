560c92208735e2e84c2f62da50c0ad969c22e39b
Kirill Birger <kbirger@gmail.com>
2025-11-11 18:07:38 -0500

    --wip-- [skip ci]

---
+++ WinTabberUI/Services/WindowSelectorStateService.cs
@@ -0,0 +1,57 @@
+﻿using AutomaticInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WinTabber.Events;
+using WinTabberUI.Extensions;
+
+namespace WinTabberUI.Services
+{
+    [GenerateAutomaticInterface]
+    public class WindowSelectorStateService(WinTabberEventManager manager)  :  IWindowSelectorStateService
+    {
+        private readonly WinTabberEventManager _manager = manager;
+
+        public IObservable<bool> GetIsEditingChanges()
+        {
+            return _manager.CommandEvents
+            .Where(evt => evt.Type == EventType.EditingStateChanged)
+            .OfType<WinTabberEvent<bool>>()
+            .Select(evt => evt.Arg)
+            .StartWith(false)
+            .Replay(1)
+            .RefCount()
+            .ObserveOnDispatcher();
+        }
+
+        public IObservable<bool> GetWindowChanges()
+        {
+            return _manager.CommandEvents
+            .Where(evt => evt.Type.IsOneOf(EventType.CmdNextWindow, EventType.CmdPreviousWindow, EventType.CmdAppHide, EventType.WindowSelected))
+            .WithLatestFrom<WinTabberEvent, bool, (WinTabberEvent CommandEvent, bool IsEditing)>(GetIsEditingChanges(), (command, isEditing) => (command, isEditing))
+            .Select(evt =>
+            {
+                var command = evt.CommandEvent;
+                var isEditing = evt.IsEditing;
+                return command.Type switch
+                {
+                    EventType.CmdNextWindow => true,
+                    EventType.CmdPreviousWindow => true,
+                    EventType.WindowSelected => false,
+                    EventType.CmdAppHide => isEditing,
+                    _ => throw new InvalidOperationException()
+                };
+            })
+            .StartWith(false)
+            .DistinctUntilChanged()
+            .Replay(1)
+            .RefCount()
+            .ObserveOnDispatcher();
+        }
+    }
+}
---
+++ WinTabberUI/Updaters/WindowSwitcherUpdater.cs
@@ -0,0 +1,15 @@
+﻿using AutomaticInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTabberUI.Updaters
+{
+
+    [GenerateAutomaticInterface]
+    public class WindowSwitcherUpdater
+    {
+    }
+}
